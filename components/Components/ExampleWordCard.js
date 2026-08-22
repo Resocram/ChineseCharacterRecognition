747dd15bff0d48e558850c4e5bf3b4d9644ffeab
Resocram <marcoser2000@gmail.com>
2026-08-22 14:02:57 -0700

    Fix wordbank words, duplicates in example and usages in definiton

--- components/Components/ExampleWordCard.js
+++ components/Components/ExampleWordCard.js
@@ -6,11 +6,11 @@ export default function ExampleWordCard(props) {
         tradAnswer = answers[answers.indexOf("(") + 1]
     }
     
-    const def = props.definition.replace(simpAnswer, "__").replace(tradAnswer, "__");
+    const def = props.definition.replaceAll(simpAnswer, "__").replaceAll(tradAnswer, "__");
     
     return (
         <div className="example-word">
-            <span className="char">{props.exampleWordChar.replace(simpAnswer, "__").replace(tradAnswer, "__")}</span>
+            <span className="char">{props.exampleWordChar.replaceAll(simpAnswer, "__").replaceAll(tradAnswer, "__")}</span>
             <span className="def">{props.pinyin} {def}</span>
         </div>
     );
